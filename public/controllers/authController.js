9a0f42e99171745849b0226935fcc43b8881d3fc
itaytal <itaytal43@gmail.com>
2022-07-29 16:39:27 +0300

    fixemail

--- public/controllers/authController.js
+++ public/controllers/authController.js
@@ -6,7 +6,7 @@ exports.login =   (req, res, next) => {
 
     const body = req.body
 
-    if(body.email === "barakbunhur@gmail.com" && body.password === "12345678"){
+    if(body.email === "barakbenhur@gmail.com" && body.password === "12345678"){
 
         const token = jwt.sign({id: 1, email: body.email}, jwtSecrt, {expiresIn: "90d"})
         res.status(200).json({status: "succsess", data: {token: token}  });
