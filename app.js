04f54ca506540e437e72de0a19998e0d50e6003b
itaytal <itaytal43@gmail.com>
2022-07-29 15:14:32 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const express = require('express');
+const authController = require('./public/controllers/authController');
+const compression = require('compression');
+
+
+const app = express();
+
+app.use(express.json());
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}\\public\\data\\tours.json`));
+
+
+app.use(compression())
+
+
+
+//ROUTES
+const tourRouter = express.Router();
+const userRouter = express.Router();
+tourRouter.get('', (req, res) => {    
+    res.status(200).json({status: "succsess" , data: tours });
+});
+
+
+
+userRouter.get('/', (req, res) => { 
+    res.status(200).json({status: "succsess"  });
+})
+
+userRouter.post('/login', authController.login)
+
+app.use('/api/v1/tours/', tourRouter);
+app.use('/api/v1/users/', userRouter);
+
+
+const port =  3000;
+app.listen(port, () => {
+
+})
---
+++ public/controllers/authController.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const jwtSecrt = "THE_ONE_TOKEN";
+
+exports.login =   (req, res, next) => {
+
+    const body = req.body
+
+    if(body.email === "barakbunhur@gmail.com" && body.password === "12345678"){
+
+        const token = jwt.sign({id: 1, email: body.email}, jwtSecrt, {expiresIn: "90d"})
+        res.status(200).json({status: "succsess", data: {token: token}  });
+    }
+    else {
+        res.status(401).json({status: "failed"})
+    }
+
+}
